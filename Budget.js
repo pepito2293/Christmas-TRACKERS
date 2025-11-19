c0c2ed3beac566aebd1641cf3c257d3afb98a5f4
pepito2293 <pepito.gaming22@gmail.com>
2025-11-19 09:58:15 +0100

    Create Budget.js with budget schema definition

---
+++ Budget.js
@@ -0,0 +1,17 @@
+{
+  "name": "Budget",
+  "type": "object",
+  "properties": {
+    "devise": {
+      "type": "string",
+      "default": "\u20ac",
+      "description": "Devise utilis\u00e9e"
+    },
+    "budget_total": {
+      "type": "number",
+      "default": 0,
+      "description": "Budget total pour No\u00ebl"
+    }
+  },
+  "required": []
+}
