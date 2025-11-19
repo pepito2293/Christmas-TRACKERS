63c6d63ef240647fd0cd20f9dd15e8bd395a974e
pepito2293 <pepito.gaming22@gmail.com>
2025-11-19 09:52:30 +0100

    Add ChristmasWreath component with image

---
+++ ChristmasWreath.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function ChristmasWreath() {
+  return (
+    <div className="relative w-32 h-32">
+      <img 
+        src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/691b3d1ac530c6dd1e685028/2b239232a_Fichier22x.png"
+        alt="Joyeux Noël"
+        className="w-full h-full object-contain"
+      />
+    </div>
+  );
+}
